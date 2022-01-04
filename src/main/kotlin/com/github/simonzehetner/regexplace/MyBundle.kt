f637fe77169b1edc1bfea079b1b110b947e561fb
GitHub Action <action@github.com>
2022-01-04 20:17:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/simonzehetner/regexplace/MyBundle.kt
+++ src/main/kotlin/com/github/simonzehetner/regexplace/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.simonzehetner.regexplace
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/simonzehetner/regexplace/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/simonzehetner/regexplace/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.simonzehetner.regexplace.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.simonzehetner.regexplace.MyBundle
 
 class MyApplicationService {
 
